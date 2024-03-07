12b813bf57ea413b59100166a34eeb784d80b879
lonshakova <lonshakova.ae@edu.spbstu.ru>
2024-03-07 14:37:48 +0300

    12

---
+++ src/components/CardMovie.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search-movie">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/SearchMovie.vue
+++ src/components/SearchMovie.vue
@@ -1,36 +1,71 @@
 <template>
   <div class="search-movie">
-    <v-text-field class="search" variant="regular"><span class="material-symbols-outlined">
+    <div class="search">
+      <v-text-field class="text-h1" placeholder="Введите название фильма..." variant="plain"></v-text-field>
+      <span class="material-symbols-outlined" @click="console.log(2)">
         search
-      </span></v-text-field>
+      </span>
+    </div>
+    <div class="select">
+      <v-select :items="items" item-title="name" variant="plain"></v-select>
   </div>
+</div>
+    
 </template>
 
 <script>
 
 export default {
-
+data(){
+  return {
+    items: [
+        { value: "title", name: "По названию" },
+        { value: "year", name: "По году" },
+        { value: "rating", name: "По рейтингу" },
+        { value: "timing", name: "По длительности" }
+      ],
+  }
+}
 };
 </script>
 
-<style>
-.search-movie {
+<style scoped>
+.search-movie{
   margin-top: 120px;
   margin-left: 74px;
-  border-radius: 30px;
+  width: 1000px;
+  display: flex;
+  
 }
-
 .search {
-  height: 65px;
-  width: 640px;
+  padding: 10px;
+  border-radius: 30px;
+  height: 45px;
+  width: 700px;
   background-color: #F3F7F8;
   border-radius: 30px;
   display: flex;
   justify-content: end;
+  align-items: center;
+  font-size: 36px;
 }
 
 .material-symbols-outlined {
   color: #BDC3C7;
   font-size: 36px;
+  cursor: pointer;
+}
+
+.select {
+  margin-left: 10px;
+  padding-right: 0px;
+  padding: 20px;
+  height: 45px;
+  width: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #F3F7F8;
+  border-radius: 30px;
 }
 </style>
