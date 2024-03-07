cf46d06e996e60c4b86c73d8aaf34bce8fab17a9
lonshakova <lonshakova.ae@edu.spbstu.ru>
2024-03-07 10:08:44 +0300

    t

--- src/components/SearchMovie.vue
+++ src/components/SearchMovie.vue
@@ -1,33 +1,36 @@
 <template>
-    <div class="search-movie">
-      <v-text-field class="search" variant="regular"><span class="material-symbols-outlined">
+  <div class="search-movie">
+    <v-text-field class="search" variant="regular"><span class="material-symbols-outlined">
         search
-        </span></v-text-field>
-    </div>
-  </template>
-  
-  <script>
+      </span></v-text-field>
+  </div>
+</template>
 
-  export default {
-    
-  };
-  </script>
-  
-  <style>
-    .search-movie{
-      margin-top: 120px;
-      margin-left: 74px;
-      border-radius: 30px;
-    }
-    .search{
-      height: 65px;
-      width: 640px;
-      background-color: #F3F7F8;
-      border-radius: 30px;
-      display: flex;
-      justify-content: end;
-    }
-    .material-symbols-outlined{
-      color:#BDC3C7;
-    }
-  </style>
+<script>
+
+export default {
+
+};
+</script>
+
+<style>
+.search-movie {
+  margin-top: 120px;
+  margin-left: 74px;
+  border-radius: 30px;
+}
+
+.search {
+  height: 65px;
+  width: 640px;
+  background-color: #F3F7F8;
+  border-radius: 30px;
+  display: flex;
+  justify-content: end;
+}
+
+.material-symbols-outlined {
+  color: #BDC3C7;
+  font-size: 36px;
+}
+</style>
